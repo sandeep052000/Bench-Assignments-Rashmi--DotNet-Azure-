acf4450be881d94c6947cfe3cf05ec5ed039925b
sandeep <sandeepagathi245@gmail.com>
2022-11-18 13:13:04 +0530

    first commit

---
+++ 1-CSharpIntroductionProgrammingAssignment/Area and Circumference of a Circle/Area and Circumference of a Circle/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_and_Circumference_of_a_Circle
+{
+    public class Class1
+    {
+        double r, Area, Circumference;
+        const double PI = 3.14;
+
+        public void AreaandCircumference()
+        {
+            Console.WriteLine("\nEnter the Radius:");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Area = PI * r * r;
+
+            Console.WriteLine("\nArea of the circle is =" + Area);
+
+            Circumference = 2 * PI * r;
+
+            Console.WriteLine("\nCircumference of the Circle is=" + Circumference);
+        }
+    }
+}
---
+++ 1-CSharpIntroductionProgrammingAssignment/Area and Circumference of a Circle/Area and Circumference of a Circle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_and_Circumference_of_a_Circle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Class1 ac = new Class1();
+            ac.AreaandCircumference();
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
